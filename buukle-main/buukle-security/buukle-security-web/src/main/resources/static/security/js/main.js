6e3ccae31c316a48bc2c2a34e330e03bd86a99dd
elvin <18001268330@163.com>
2019-08-18 04:56:44 +0800

    任务中心,消息中心初步设计

--- buukle-main/buukle-security/buukle-security-web/src/main/resources/static/security/js/main.js
+++ buukle-main/buukle-security/buukle-security-web/src/main/resources/static/security/js/main.js
@@ -29,35 +29,10 @@ layui.use(['layer', 'form', 'element', 'jquery', 'dialog'], function() {
 		mainLayout.removeClass('hide-side');
 	});
 	//监听导航点击
-	/*element.on('nav(leftNav)', function(elem) {
-		var navA = $(elem).find('a');
-		var id = navA.attr('data-id');
-		var url = navA.attr('data-url');
-		var text = navA.attr('data-text');
-		if(!url){
-			return;
-		}
-		var isActive = $('.main-layout-tab .layui-tab-title').find("li[lay-id=" + id + "]");
-		if(isActive.length > 0) {
-			//切换到选项卡
-			element.tabChange('tab', id);
-		} else {
-			element.tabAdd('tab', {
-				title: text,
-				content: '<iframe src="' + url + '" name="iframe' + id + '" class="iframe" framborder="0" data-id="' + id + '" scrolling="auto" width="100%"  height="100%"></iframe>',
-				id: id
-			});
-			element.tabChange('tab', id);
-			
-		}
-		mainLayout.removeClass('hide-side');
-	});*/
-	//监听导航点击
 	element.on('nav(rightNav)', function(elem) {
-		var navA = $(elem).find('a');
-		var id = navA.attr('data-id');
-		var url = navA.attr('data-url');
-		var text = navA.attr('data-text');
+		var id = elem.attr('data-id');
+		var url = elem.attr('data-url');
+		var text = elem.attr('data-text');
 		if(!url){
 			return;
 		}
